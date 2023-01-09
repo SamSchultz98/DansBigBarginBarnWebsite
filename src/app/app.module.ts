688c33740cca459979e5ef3dfdf678b358ec0cc8
SamSchultz98 <sam.schultz@outlook.com>
2023-01-09 17:57:45 -0500

    E404ErrorComponentCreated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './misc/home/home.component';
+import { E404Component } from './misc/e404/e404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    E404Component
   ],
   imports: [
     BrowserModule,
